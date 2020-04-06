1f941af5683f72977c01cd106d7f6b9db271d3c2
ravi edara <r.edara@outlook.com>
2020-04-06 21:58:31 +0530

    working website ui code

--- SimhapuriServices.Web/ClientApp/src/app/app.module.ts
+++ SimhapuriServices.Web/ClientApp/src/app/app.module.ts
@@ -1,34 +1,107 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ReactiveFormsModule } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { PageLoaderComponent } from './layout/page-loader/page-loader.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { RightSidebarComponent } from './layout/right-sidebar/right-sidebar.component';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+import { DynamicScriptLoaderService } from './services/dynamic-script-loader.service';
+import { RightSidebarService } from './services/rightsidebar.service';
+import { WINDOW_PROVIDERS } from './services/window.service';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { NgxMaskModule } from 'ngx-mask';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatDialogModule } from '@angular/material/dialog';
+import { SimpleDialogComponent } from './ui/modal/simpleDialog.component';
+import { DialogformComponent } from './ui/modal/dialogform/dialogform.component';
+import { BottomSheetOverviewExampleSheet } from './ui/bottom-sheet/bottom-sheet.component';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { AgmCoreModule } from '@agm/core';
+import { HttpClientModule } from '@angular/common/http';
+import { AdvanceTableService } from '../app/tables/advance-table/advance-table.service';
+import {
+  PerfectScrollbarModule,
+  PERFECT_SCROLLBAR_CONFIG,
+  PerfectScrollbarConfigInterface
+} from 'ngx-perfect-scrollbar';
+
+const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
+  suppressScrollX: true,
+  wheelPropagation: false
+};
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent
+    HeaderComponent,
+    PageLoaderComponent,
+    SidebarComponent,
+    RightSidebarComponent,
+    SimpleDialogComponent,
+    DialogformComponent,
+    BottomSheetOverviewExampleSheet
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    NgbModule,
     HttpClientModule,
+    ReactiveFormsModule,
     FormsModule,
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
-    ])
+    PerfectScrollbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatProgressBarModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatListModule,
+    MatSidenavModule,
+    MatButtonToggleModule,
+    MatCheckboxModule,
+    MatSlideToggleModule,
+    MatMenuModule,
+    NgxMaskModule.forRoot(),
+    AgmCoreModule.forRoot({
+      apiKey: 'YOUR API KEY'
+    })
   ],
-  providers: [],
+  providers: [
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
+    {
+      provide: PERFECT_SCROLLBAR_CONFIG,
+      useValue: DEFAULT_PERFECT_SCROLLBAR_CONFIG
+    },
+    DynamicScriptLoaderService,
+    RightSidebarService,
+    AdvanceTableService,
+    WINDOW_PROVIDERS
+  ],
+  entryComponents: [
+    SimpleDialogComponent,
+    DialogformComponent,
+    BottomSheetOverviewExampleSheet
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
